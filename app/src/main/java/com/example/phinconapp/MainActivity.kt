79892536ed3029a4f771fc017b7c91446e617b76
kishibee <raflyade9@gmail.com>
2024-08-28 22:01:43 +0700

    idea

--- app/src/main/java/com/example/phinconapp/MainActivity.kt
+++ app/src/main/java/com/example/phinconapp/MainActivity.kt
@@ -30,18 +30,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//note safe app where users can feel safe to dump they feeling by writing it in this app
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhinconAppTheme {
-        Greeting("Android")
-    }
-}
+//after splash screen there will be a screen to check percentage of emotion user have at the moment
+//you cant skip this screen because we encourage user to tell a story everyday/every they open the app
+
+//after that, straight into the note screen where user can tell a story/event that make user happy/sad
+
+//and after tell a story, home screen where you can track/check what emotions/story you have on other day
+
+//CRUD
